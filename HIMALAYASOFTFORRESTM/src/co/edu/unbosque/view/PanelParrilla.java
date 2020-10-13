e27211cdfdd5d0a519e9de80475efadddf9ee62c
Nicolas-Pena-Mogollon <71480273+Nicolas-Pena-Mogollon@users.noreply.github.com>
2020-10-12 20:48:48 -0500

    12/10/2020 - 8:48 Modificación de la clase PanelAgregarCancion

--- HIMALAYASOFTFORRESTM/src/co/edu/unbosque/view/PanelParrilla.java
+++ HIMALAYASOFTFORRESTM/src/co/edu/unbosque/view/PanelParrilla.java
@@ -3,7 +3,6 @@ package co.edu.unbosque.view;
 import java.awt.BorderLayout;
 import java.awt.Color;
 
-
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 
@@ -24,45 +23,46 @@ public class PanelParrilla extends JPanel {
 	public PanelParrilla() {
 
 		setBackground(Color.white);
-		setBorder( new TitledBorder( "Parrilla Musical" ) ) ;
+		setBorder(new TitledBorder("Parrilla Musical"));
 		setLayout(new BorderLayout());
-		//asignarValores();
-		//crearTabla(String[][] datos);
-		//agregarComponentes();
-		
+		// asignarValores();
+		// crearTabla(String[][] datos);
+		// agregarComponentes();
+
 	}
+
 	/**
 	 * 
-	 * @author Juana Valentina Torres Parrado
-	 * Se obtiene un String bidimencional para generar la tabla de la parrilla 
+	 * @author Juana Valentina Torres Parrado Se obtiene un String bidimencional
+	 *         para generar la tabla de la parrilla
 	 * @param datos
 	 * 
 	 */
 	public void crearTabla(String[][] datos) {
-		String [] dias= {"Cancion","Autor","Genero"};
-		tablaParrilla = new JTable(datos,dias);
-		sp= new JScrollPane (tablaParrilla);
-		
-		
+		String[] dias = { "Cancion", "Autor", "Genero" };
+		tablaParrilla = new JTable(datos, dias);
+		sp = new JScrollPane(tablaParrilla);
+
 	}
+
 	/**
 	 * 
-	 * @author Juana Valentina Torres Parrado
-	 * Se asigna los valores correspondientes a cada componente
+	 * @author Juana Valentina Torres Parrado Se asigna los valores correspondientes
+	 *         a cada componente
 	 * 
 	 */
 	public void asignarValores() {
-        
-		botonAgregar= new JButton("Agregar");
-		comboNombreCancion= new JComboBox();
+
+		botonAgregar = new JButton("Agregar");
+		comboNombreCancion = new JComboBox();
 		comboNombreCancion.addItem("NombreCanciones");
-			
-		
+
 	}
+
 	/**
 	 * 
-	 * @author Juana Valentina Torres Parrado
-	 * Se agrega cada componente del panel y se orienta en sur, centro y este.
+	 * @author Juana Valentina Torres Parrado Se agrega cada componente del panel y
+	 *         se orienta en sur, centro y este.
 	 * 
 	 */
 	public void agregarComponentes() {
@@ -71,6 +71,40 @@ public class PanelParrilla extends JPanel {
 		add(botonAgregar, BorderLayout.EAST);
 	}
 
-	
-	
+	public JButton getBotonAgregar() {
+		return botonAgregar;
+	}
+
+	public void setBotonAgregar(JButton botonAgregar) {
+		this.botonAgregar = botonAgregar;
+	}
+
+	public JTable getTablaParrilla() {
+		return tablaParrilla;
+	}
+
+	public void setTablaParrilla(JTable tablaParrilla) {
+		this.tablaParrilla = tablaParrilla;
+	}
+
+	public JComboBox getComboNombreCancion() {
+		return comboNombreCancion;
+	}
+
+	public void setComboNombreCancion(JComboBox comboNombreCancion) {
+		this.comboNombreCancion = comboNombreCancion;
+	}
+
+	public JScrollPane getSp() {
+		return sp;
+	}
+
+	public void setSp(JScrollPane sp) {
+		this.sp = sp;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
 }
