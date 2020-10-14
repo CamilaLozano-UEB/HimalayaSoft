f45e24e30c4e3ae759c40a21a701afcb9e340424
jvtp0 <jvtp0@LAPTOP-A92LBN28>
2020-10-14 10:34:57 -0500

    14/10/20 10:34am se modifica controller ya que faltaban sus atributos y constructor

--- HIMALAYASOFTFORRESTM/src/co/edu/unbosque/controller/Controller.java
+++ HIMALAYASOFTFORRESTM/src/co/edu/unbosque/controller/Controller.java
@@ -3,8 +3,18 @@ package co.edu.unbosque.controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import co.edu.unbosque.model.Emisora;
+import co.edu.unbosque.view.View;
+
 public class Controller implements ActionListener {
 
+	private View vista;
+	private Emisora emisora;
+	
+	public Controller() {
+		vista=new View(this);
+		emisora= new Emisora();
+	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
