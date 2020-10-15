8aa6c4f8b52c6c5eafb9ad0aa1f22ce6bd8bbac7
CamilaLozano-UEB <mclozanog@unbosque.edu.co>
2020-10-15 16:51:26 -0500

    15/10/2020 Se añadio una supresión en el AplMain para el controller

--- HIMALAYASOFTFORRESTM/src/co/edu/unbosque/controller/AplMain.java
+++ HIMALAYASOFTFORRESTM/src/co/edu/unbosque/controller/AplMain.java
@@ -5,6 +5,7 @@ public class AplMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		@SuppressWarnings("unused")
 		Controller control=new Controller();
 	}
 
