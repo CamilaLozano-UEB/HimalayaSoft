dbbc8772a51a634e75d2b160aa7b91223b8a9d28
CamilaLozano-UEB <mclozanog@unbosque.edu.co>
2020-10-16 00:35:38 -0500

    Agrega constante de herencia

--- HIMALAYASOFTFORRESTM/src/co/edu/unbosque/model/ExtensionIncorrectaException.java
+++ HIMALAYASOFTFORRESTM/src/co/edu/unbosque/model/ExtensionIncorrectaException.java
@@ -2,6 +2,9 @@ package co.edu.unbosque.model;
 
 public class ExtensionIncorrectaException extends Exception {
 
+
+	private static final long serialVersionUID = 1L;
+
 	public ExtensionIncorrectaException(String mensaje) {
 		super(mensaje);
 	}
