9a47937de5cf7a489d03a9c346d88cc77743d12f
Nicolas-Pena-Mogollon <71480273+Nicolas-Pena-Mogollon@users.noreply.github.com>
2020-10-17 17:58:44 -0500

    Arreglo drop y reproductor

--- HIMALAYASOFTFORRESTM/src/co/edu/unbosque/view/PanelAgregarCancion.java
+++ HIMALAYASOFTFORRESTM/src/co/edu/unbosque/view/PanelAgregarCancion.java
@@ -209,8 +209,8 @@ public class PanelAgregarCancion extends JPanel implements DropTargetListener {
 			 * @author Nicolás Peña Mogollón
 			 *
 			 *         El método getTransferable de la clase DropTargetDropEvent retorna un
-			 *         objeto ransferable asociado con el Drop. Cuando el usuario interviene
-			 *         con el drop. Este se almacena en tr. Un objeto de la clase
+			 *         objeto Transferable asociado con el Drop. Cuando el usuario
+			 *         interviene con el drop. Este se almacena en tr. Un objeto de la clase
 			 *         Transferable para que se puedan proporcionar los datos en la
 			 *         operación de transferencia.
 			 */
@@ -278,10 +278,7 @@ public class PanelAgregarCancion extends JPanel implements DropTargetListener {
 					 *         Se da por terminado el drop
 					 */
 					dtde.dropComplete(true);
-				} else {
 					i = flavors.length;
-					JOptionPane.showMessageDialog(null, "Error al insetar imagen!!!", "Warning",
-							JOptionPane.WARNING_MESSAGE);
 				}
 			}
 			dtde.rejectDrop();
