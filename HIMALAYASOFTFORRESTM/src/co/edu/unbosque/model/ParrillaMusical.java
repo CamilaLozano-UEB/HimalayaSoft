4f4547d0a0a3bc9696b9b9f26b582221463746de
Nicolas-Pena-Mogollon <71480273+Nicolas-Pena-Mogollon@users.noreply.github.com>
2020-10-17 17:30:39 -0500

    Nuevo envío

--- HIMALAYASOFTFORRESTM/src/co/edu/unbosque/model/ParrillaMusical.java
+++ HIMALAYASOFTFORRESTM/src/co/edu/unbosque/model/ParrillaMusical.java
@@ -37,6 +37,8 @@ public class ParrillaMusical {
 
 	public void borrarPlayList() {
 		this.reproductorParrilla.getPlayList().clear();
+		if(this.reproductorParrilla.getPlayList().isEmpty()) {
+		}
 	}
 
 	public void reproducir() {
